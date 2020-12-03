be9f8de689811fed6bb54e551d00f722ade53cae
reddydavinci <busisatya@gmail.com>
2020-12-03 17:58:38 +0530

    HashMap added

--- src/com/lti/Collections/Map/HasheMap.java
+++ src/com/lti/Collections/Map/HasheMap.java
@@ -6,12 +6,30 @@ import java.util.*;
  * Created by busis on 2020-12-03.
  */
 public class HasheMap {
+    //Find who all passed
     public List<String> getPassList(Map<String,Integer> input){
         List<String> ans = new ArrayList<String>();
         Set<String> ks = input.keySet();
         for(String s:ks){
             if(input.get(s)>70)
                 ans.add(s);
+        }
+
+        return ans;
+    }
+
+    //Find occurances of digits
+    public Map<Character,Integer> getFrequency(String input){
+        Map<Character,Integer> ans = new HashMap<Character, Integer>();
+        for(int i=0;i<input.length();i++){
+            if(ans.containsKey(input.charAt(i))){
+                int n=ans.get(input.charAt(i));
+                ans.put(input.charAt(i),n+1);
+
+            }
+            else{
+                ans.put(input.charAt(i),1);
+            }
         }
         return ans;
     }
@@ -46,5 +64,7 @@ public class HasheMap {
         map1.put("Kichu",78);
         map1.put("Drogs",56);
         System.out.println(new HasheMap().getPassList(map1));
+
+        System.out.println(new HasheMap().getFrequency("apple"));
     }
 }
