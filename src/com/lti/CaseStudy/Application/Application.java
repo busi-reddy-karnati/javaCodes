0e7c4cd8143a7a5e5b9d3212b40913f9ccbde934
reddydavinci <busisatya@gmail.com>
2020-12-05 21:27:26 +0530

    Methods for taking details added

--- src/com/lti/CaseStudy/Application/Application.java
+++ src/com/lti/CaseStudy/Application/Application.java
@@ -1,5 +1,13 @@
 package com.lti.CaseStudy.Application;
 
+import com.lti.CaseStudy.CaseStudy2.AppEngine;
+import com.lti.CaseStudy.CaseStudyPlan.Course;
+import com.lti.CaseStudy.CaseStudyPlan.Student;
+
+import java.sql.SQLException;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.Scanner;
 
 /**
@@ -19,49 +27,148 @@ class Implementation implements Application{
 
     @Override
     public void showFirstScreen() {
-        System.out.println("Welcome to SMS(Student Mgmt. System) v1.0");
-        System.out.println("Tell us who you are : \n1. Student\n2. Admin");
-        System.out.println("Enter your choice ( 1 or 2 ) : ");
+        System.out.println("Hello there!");
+        System.out.println("Press 1: Admin Page");
+        System.out.println("2: Student Page");
+        int option;
         Scanner scanner = new Scanner(System.in);
-        int op = Integer.parseInt(scanner.nextLine());
-
-        switch(op){
+        option = scanner.nextInt();
+        switch (option){
             case 1:
                 showStudentScreen();
                 break;
             case 2:
                 showAdminScreen();
                 break;
+            default:
+                System.out.println("Invalid Entry. Please try again!");
         }
     }
 
     @Override
     public void showStudentScreen() {
-
+        System.out.println("This is the Student Screen: No functionality as of now");
     }
 
     @Override
     public void showAdminScreen() {
+        while(true){
+            System.out.println("Press 1: Show all Courses");
+            System.out.println("2: Show all Students");
+            System.out.println("3: Register a Student");
+            System.out.println("4: Introduce a course");
+            System.out.println("0: EXIT");
+            int option;
+            Scanner scanner = new Scanner(System.in);
+            option = scanner.nextInt();
+            switch (option){
+                case 1:
+                    showAllCoursesScreen();
+                    break;
+                case 2:
+                    showAllStudentsScreen();
+                    break;
+                case 3:
+                    showStudentRegistrationScreen();
+                    break;
 
+                case 4:
+                    introduceNewCourseScreen();
+                    break;
+
+                case 0:
+                    System.exit(0);
+                    default:
+                        System.out.println("Invalid option entered. Try Again");
+            }
+        }
     }
 
     @Override
     public void showAllStudentsScreen() {
-
+        AppEngine appEngine = new AppEngine();
+        try {
+            Student[] students = appEngine.listOfStudents();
+            for(Student student:students){
+                System.out.println("Name: "+student.getName());
+                System.out.println("Id: "+student.getId());
+                System.out.println("dob: "+student.getDateOfBirth());
+            }
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return;
     }
 
     @Override
     public void showStudentRegistrationScreen() {
-
+        Scanner sc=new Scanner(System.in);
+        String name;
+        int id;
+        Date d =new Date();
+        SimpleDateFormat sdf = new SimpleDateFormat("dd-MM-YYYY");
+        System.out.println("Give ID, Name, DOB(in DD-MM-YYYY FORMAT)");
+        id=sc.nextInt();
+        name = sc.next();
+        String dob = sc.next();
+        try {
+            d=sdf.parse(dob);
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+        Student student = new Student(id,name,d);
+        AppEngine appEngine = new AppEngine();
+        try {
+            appEngine.register(student);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+        return;
     }
 
     @Override
     public void introduceNewCourseScreen() {
-
+        Scanner sc = new Scanner(System.in);
+        String courseName;
+        int courseId, duration, fees;
+        //idnamedurationfees
+        System.out.println("Give ID, Name, Duration, Fees for the course");
+        courseId = sc.nextInt();
+        courseName = sc.next();
+        duration=sc.nextInt();
+        fees=sc.nextInt();
+        Course course = new Course(courseId,courseName,duration,fees);
+        AppEngine appEngine = new AppEngine();
+        try {
+            appEngine.introduce(course);
+        }
+        catch (SQLException e){
+            e.printStackTrace();
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
     public void showAllCoursesScreen() {
+        AppEngine appEngine = new AppEngine();
+        try {
+            Course[] courses=appEngine.listOfCourses();
+            for(Course course:courses){
+                System.out.println("Name: "+course.getName());
+                System.out.println("Id: "+course.getId());
+                System.out.println("Fees: "+course.getFees());
+                System.out.println("Duration: "+course.getDuration()+" months");
+            }
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
 
     }
 }
--- src/com/lti/CaseStudy/CaseStudyPlan/App.java
+++ src/com/lti/CaseStudy/CaseStudyPlan/App.java
@@ -12,17 +12,10 @@ import java.util.Scanner;
  */
 public class App {
     public void scenario1(){
-        Student s1 = new Student(1,"Sachin","1973-04-24");
-        Student s2 = new Student(2,"Sehwag","1978-10-20");
-        Student s3 = new Student(3,"Ganguly","1972-07-08");
-        Student s4 = new Student(4,"Laxman","1974-11-01");
+
     }
     public void scenario2(){
         List<Student> students = new ArrayList<Student>();
-        students.add(new Student(1,"Sachin2","1973-04-24"));
-        students.add(new Student(2,"Sehwag2","1978-10-20"));
-        students.add(new Student(3,"Ganguly2","1972-07-08"));
-        students.add(new Student(4,"Laxman2","1974-11-01"));
         for(Student student : students){
             new Info().display(student);
         }
@@ -35,7 +28,7 @@ public class App {
         String name = sc.next();
         System.out.println("DOB: ");
         String dob = sc.next();
-        Student student = new Student(id,name,dob);
+
 
     }
     public static void main(String[] args) {
