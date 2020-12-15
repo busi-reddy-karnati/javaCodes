ca958c56a59838c83600988ceb8d8eef50a0b1ec
reddydavinci <busisatya@gmail.com>
2020-12-15 23:13:40 +0530

    Finding the Chocolates . Type casted to get the correct answer

--- src/com/lti/Algorithms/Codility/EuclideanAlgorithm/ChocolateByNumbers.java
+++ src/com/lti/Algorithms/Codility/EuclideanAlgorithm/ChocolateByNumbers.java
@@ -1,27 +1,26 @@
 package com.lti.Algorithms.Codility.EuclideanAlgorithm;
 
-import java.util.HashSet;
-import java.util.Set;
-
 /**
  * Created by busis on 2020-12-14.
  */
 public class ChocolateByNumbers {
     public static void main(String[] args) {
-        System.out.println(solution(10,4));
+        System.out.println(solution(947853, 4453));
+    }
+    public static int gcd(int a, int b){
+        if(b==0)
+            return a;
+        int ans=gcd(b,a%b);
+        return gcd(b,a%b);
+    }
+    public static long lcm(int a,int b){
+        int g=gcd(a,b);
+        //Multiplying two int gives int s, type cast them to be able to get long
+        long ans=((long)((long)a*(long)b)/(long)g);
+        return ans;
     }
     public static int solution(int n, int m){
-        if(m==1)
-            return n;
-
-        Set<Integer> set=new HashSet<Integer>();
-        int num=0;
-        int ans=0;
-        while(!set.contains(num)){
-            set.add(num);
-            ans+=1;
-            num=(num+m)%n;
-        }
-        return ans;
+        long ans=lcm(n,m)/m;
+        return (int)ans;
     }
 }
