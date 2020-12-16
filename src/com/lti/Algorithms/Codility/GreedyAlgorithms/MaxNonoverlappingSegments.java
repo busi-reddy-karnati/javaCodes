0ab200d68bf86d3980f455ab3a5f9f3c055519c1
reddydavinci <busisatya@gmail.com>
2020-12-16 15:09:26 +0530

    Max Non Overlapping Greedy

---
+++ src/com/lti/Algorithms/Codility/GreedyAlgorithms/MaxNonoverlappingSegments.java
@@ -0,0 +1,7 @@
+package com.lti.Algorithms.Codility.GreedyAlgorithms;
+
+/**
+ * Created by busis on 2020-12-16.
+ */
+public class MaxNonoverlappingSegments {
+}
---
+++ src/com/lti/Algorithms/Codility/PrimeAndCompositeNumbers/CountNondivisible.java
@@ -0,0 +1,7 @@
+package com.lti.Algorithms.Codility.PrimeAndCompositeNumbers;
+
+/**
+ * Created by busis on 2020-12-14.
+ */
+public class CountNondivisible {
+}
