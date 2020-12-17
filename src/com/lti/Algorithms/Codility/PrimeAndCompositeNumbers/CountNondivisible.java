501df8cf56afb6816c4683583441a9a58c320d67
reddydavinci <busisatya@gmail.com>
2020-12-17 11:09:02 +0530

    Clean

--- src/com/lti/Algorithms/Codility/PrimeAndCompositeNumbers/CountNondivisible.java
+++
@@ -1,7 +0,0 @@
-package com.lti.Algorithms.Codility.PrimeAndCompositeNumbers;
-
-/**
- * Created by busis on 2020-12-14.
- */
-public class CountNondivisible {
-}
--- src/com/lti/Algorithms/Codility/Sorting/MaxproductOfThree.java
+++ src/com/lti/Algorithms/Codility/Sorting/MaxproductOfThree.java
@@ -1,18 +1,23 @@
 package com.lti.Algorithms.Codility.Sorting;
 
 import java.util.Arrays;
-
-/**
- * Created by busis on 2020-12-13.
+/*
+sort the array
+There can be three cases
+all positives, all negatives, a mix
+in all negatives, it would be the last three
+in all positive it would be last three
+in a mix, it would be either last three or first two(to make positive) and last one
  */
-public class UniqueThreeSum {
+public class MaxProductOfThree {
     public static void main(String[] args) {
         int[] a={-3,1,2,-2,5,6};
-        System.out.println(new UniqueThreeSum().solution(a));
+        System.out.println(new MaxProductOfThree().solution(a));
     }
     public int solution(int[] a){
         Arrays.sort(a);
-        int n=a.length;
-        return Math.max(Math.max(a[n-1]*a[n-2]*a[n-3],a[0]*a[1]*a[2]),Math.max(a[n-1]*a[n-2]*a[0],a[0]*a[1]*a[n-1]));
+        int n=a.length-1;
+        return Math.max(a[n]*a[n-1]*a[n-2],a[0]*a[1]*a[n]);
     }
+
 }
